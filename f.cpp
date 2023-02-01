410f6e69c87a2c67fd736a628dff9ccc88f8d01f
Saniya <s_mashrap@kbtu.kz>
2023-02-02 04:22:43 +0600

    initial commit

--- f.cpp
+++
@@ -1,2 +0,0 @@
-for x in range(7):
-  print(x) 
---
+++ if,else.py
@@ -0,0 +1,24 @@
+a = 33
+b = 200
+c = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+if(b == c):
+    print("b is equal to c")
+
+print("A") if a > b else print("B")
+
+
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
+if a > b:
+    pass
