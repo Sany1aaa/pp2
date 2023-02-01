7f61ddf23d73c5278d41c843a1c149588e5c45ff
Saniya <s_mashrap@kbtu.kz>
2023-02-02 01:48:16 +0600

    initial commit

---
+++ 2.py
@@ -0,0 +1,16 @@
+y = 15
+
+print(bool("Hello")) #any string is true except empty string 
+print(bool(y))
+
+bool(123) #any number кроме 0 is true
+bool(["apple", "cherry", "banana"]) # все кроме empty set is true 
+
+#The FALSE is 
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({}) 
