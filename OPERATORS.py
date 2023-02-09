46d1397dd03be7eec92788a618a310134c693cbe
Saniya <s_mashrap@kbtu.kz>
2023-02-09 12:16:48 +0600

    initial commit

--- OPERATORS.py
+++ OPERATORS.py
@@ -8,4 +8,6 @@ print(x / y) #division
 print(x % y) #modulus(калдык)
 print(x ** y) # x-тын y-дарежесы
 print(x // y) # болгендегы бутын болыгы
-#print(x &= y) # тусынюедым втф
+#print(x &= y) # тусынюедым втф 
+print(x ^ y) # xor 
+
