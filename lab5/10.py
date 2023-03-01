4bb01ddf81f3ae774299233fb508ab16b6f20216
Saniya <s_mashrap@kbtu.kz>
2023-03-02 01:02:42 +0600

    initial commit

--- lab5/10.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def camel_to_snake(string):
-    return re.sub(r'(?<!^)(?=[A-Z])', '_', string).lower()
-
-string = input("Enter the camel case string: ")
-snake_string = camel_to_snake(string)
-print("Snake case string: ", snake_string)
